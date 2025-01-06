00eca22d354b2fe567ad49172c89e3246bc7a543
Ainur Nusratullin <dr.nusartullin@yandex.ru>
2025-01-06 14:54:31 +0500

    Задание выполнено

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -7,25 +7,25 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserDaoJDBCImpl extends Util implements UserDao {
+public class UserDaoJDBCImpl implements UserDao {
 
-    Connection connection = getConnection();
+    private Util util;
 
     public UserDaoJDBCImpl() {
-
+        util = new Util();
     }
 
 
+    @Override
     public void createUsersTable() {
-        PreparedStatement ps = null;
         String sql = "CREATE TABLE IF NOT EXISTS users (" +
                 "id BIGINT AUTO_INCREMENT PRIMARY KEY," +
                 "name VARCHAR(255)," +
                 "lastname VARCHAR(255)," +
                 "age TINYINT" +
                 ")";
-        try {
-            ps = connection.prepareStatement(sql);
+        try (Connection connection = util.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -33,22 +33,23 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
 
     }
 
+    @Override
     public void dropUsersTable() {
-        PreparedStatement ps = null;
         String sql = "DROP TABLE IF EXISTS users";
-        try {
-            ps = connection.prepareStatement(sql);
+        try (Connection connection = util.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
-        PreparedStatement ps = null;
+
         String sql = "INSERT INTO users (name, lastname, age) VALUES(?, ?, ?)";
-        try {
-            ps = connection.prepareStatement(sql);
+        try (Connection connection = util.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.setString(1, name);
             ps.setString(2, lastName);
             ps.setByte(3, age);
@@ -58,11 +59,12 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
         }
     }
 
+    @Override
     public void removeUserById(long id) {
-        PreparedStatement ps = null;
+
         String sql = "DELETE FROM users WHERE id = ?";
-        try {
-            ps = connection.prepareStatement(sql);
+        try (Connection connection = util.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.setLong(1, id);
             ps.executeUpdate();
         } catch (SQLException e) {
@@ -70,13 +72,15 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
         }
     }
 
+    @Override
     public List<User> getAllUsers() {
+
         List<User> userList = new ArrayList<>();
         String sql = "SELECT * FROM users";
-        Statement statement = null;
-        try {
-            statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sql);
+        try (Connection connection = util.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+
             while (resultSet.next()) {
                 User user = new User();
                 user.setId(resultSet.getLong("id"));
@@ -92,11 +96,12 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
         return userList;
     }
 
+    @Override
     public void cleanUsersTable() {
-        Statement statement = null;
+
         String sql = "DELETE FROM users";
-        try {
-            statement = connection.createStatement();
+        try (Connection connection = util.getConnection();
+             Statement statement = connection.createStatement()) {
             statement.executeUpdate(sql);
         } catch (SQLException e) {
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,14 +1,14 @@
 package jm.task.core.jdbc.util;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/mysql";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
+    private static final String USERNAME = "jpauser";
+    private static final String PASSWORD = "jpapwd";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
 
     public static Connection getConnection() {
         Connection connection = null;
