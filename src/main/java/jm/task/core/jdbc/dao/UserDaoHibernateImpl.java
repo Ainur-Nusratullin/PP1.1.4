d64120e2fc533caa5d2ae9380b3f505787b849c2
Ainur Nusratullin <dr.nusratullin@yandex.ru>
2025-01-09 18:12:15 +0500

    Вечер

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -19,43 +19,108 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        Session session = util.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
+        Transaction transaction = null;
+        try (Session session = util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
 
-        String sql = "CREATE TABLE IF NOT EXISTS users " +
-                "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
-                "name VARCHAR(50) NOT NULL, lastName VARCHAR(50) NOT NULL, " +
-                "age TINYINT NOT NULL)";
+            String hql = "CREATE TABLE IF NOT EXISTS users " +
+                    "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                    "name VARCHAR(50) NOT NULL, lastName VARCHAR(50) NOT NULL, " +
+                    "age TINYINT NOT NULL)";
 
-        Query query = session.createSQLQuery(sql).addEntity(User.class);
+            Query query = session.createSQLQuery(hql).addEntity(User.class);
+            query.executeUpdate();
 
-        transaction.commit();
-        session.close();
-
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
+        Transaction transaction = null;
+        try (Session session = util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
 
+            String hql = "DROP TABLE IF EXISTS users";
+
+            Query query = session.createSQLQuery(hql).addEntity(User.class);
+            query.executeUpdate();
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Transaction transaction = null;
+        try (Session session = util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
 
+            User user = new User(name, lastName, age);
+            session.save(user);
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        Transaction transaction = null;
+        try (Session session = util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
 
+            User user = (User) session.get(User.class, id);
+            session.delete(user);
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        try (Session session = util.getSessionFactory().openSession()) {
+            return session.createQuery("from user", User.class).list();
+
+        }
     }
 
     @Override
     public void cleanUsersTable() {
+        Transaction transaction = null;
+        try (Session session = util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
 
+            String hql = "DELETE FROM users";
+
+            Query query = session.createSQLQuery(hql).addEntity(User.class);
+            query.executeUpdate();
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 }
